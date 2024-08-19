6fdd40446ee07db393ee34dc4a9d556e6b30416b
alian <alianmc1988@gmail.com>
2024-08-19 18:35:01 -0300

    fix: comments

--- base/config.go
+++ base/config.go
@@ -51,39 +51,3 @@ func GetConfig() *Config {
 	}
 	return singleInstance	
 }
-
-
-
-// func Init() ConfigType {
-// 	err:= godotenv.Load()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	var (
-// 	Port = os.Getenv("PORT")
-// 	Host = os.Getenv("HOST")
-// 	BaseURL = os.Getenv("BASE_URL")
-// 	DbHost = os.Getenv("DB_HOST")
-// 	DbPort = os.Getenv("DB_PORT")
-// 	DbUser = os.Getenv("DB_USER")
-// 	DbPassword = os.Getenv("DB_PASSWORD")
-// 	DbName = os.Getenv("DB_NAME")
-// 	DbSSLMode = os.Getenv("DB_SSL_MODE")
-// 	JwtSecret = os.Getenv("JWT_SECRET")
-// )
-// 	return ConfigType{
-// 		"PORT": Port,
-// 		"HOST": Host,
-// 		"BASE_URL": BaseURL,
-// 		"DB_HOST": DbHost,
-// 		"DB_PORT": DbPort,
-// 		"DB_USER": DbUser,
-// 		"DB_PASSWORD": DbPassword,
-// 		"DB_NAME": DbName,
-// 		"DB_SSL_MODE": DbSSLMode,
-// 		"JWT_SECRET": JwtSecret,
-// 	}
-// }
-
-// var Config = Init()
